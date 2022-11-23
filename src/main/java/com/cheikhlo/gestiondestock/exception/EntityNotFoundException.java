b62f453db582907a6f6c7498e356c4ba9ec41c69
chheikhlo <chheikhlo@gmail.com>
2022-11-23 17:53:53 +0100

    gestion d'exception

---
+++ src/main/java/com/cheikhlo/gestiondestock/exception/EntityNotFoundException.java
@@ -0,0 +1,29 @@
+package com.cheikhlo.gestiondestock.exception;
+
+import lombok.Getter;
+
+//Si je cherche un article par son code et que je le trouve pas cette arcticle , je dois envoyé une exception de type EntityNotFound
+public class EntityNotFoundException extends RuntimeException {
+
+  @Getter
+  private ErrorCodes errorCode;
+
+  public EntityNotFoundException(String message) {
+    super(message);
+  }
+
+  public EntityNotFoundException(String message, Throwable cause /*pour logger la cause*/ ) {
+    super(message, cause);
+  }
+
+  public EntityNotFoundException(String message, Throwable cause, ErrorCodes errorCode) {
+    super(message, cause);
+    this.errorCode = errorCode;
+  }
+
+  public EntityNotFoundException(String message, ErrorCodes errorCode) {
+    super(message);
+    this.errorCode = errorCode;
+  }
+
+}
